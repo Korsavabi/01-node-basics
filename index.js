ce0961f4976497058c636a5acff55499f6df68dc
Korsavabi <korsavabi15@gmail.com>
2021-01-20 10:31:29 +0200

    homework

---
+++ index.js
@@ -0,0 +1,26 @@
+let a = 10;
+console.log('a ', a);
+
+// contacts.js
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+// function listContacts() {
+//     // ...твой код
+//   }
+  
+//   function getContactById(contactId) {
+//     // ...твой код
+//   }
+  
+//   function removeContact(contactId) {
+//     // ...твой код
+//   }
+  
+//   function addContact(name, email, phone) {
+//     // ...твой код
+//   }
